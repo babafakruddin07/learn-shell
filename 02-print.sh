8722c8dd58d7be89bedc5d22da7ca34b5fdf7058
baba <babamtech@gmail.com>
2023-05-31 15:19:47 +0530

    first print with all colours

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,11 @@
 #to print a message a couple of commands we use echo
 
 echo hello baba fakruddin
+#syntax : echo -e "\e[COLmMESSAGE\e[0m"
+
+echo -e "\e[31mhi baba fakruddin\e[0m"
+echo -e "\e[32mhi baba fakruddin\e[0m"
+echo -e "\e[33mhi baba fakruddin\e[0m"
+echo -e "\e[34mhi baba fakruddin\e[0m"
+echo -e "\e[35mhi baba fakruddin\e[0m"
+echo -e "\e[36mhi baba fakruddin\e[0m"
